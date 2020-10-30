bac532c3aff7c4cdbec717bec695e9092e88a172
Brian Davis <herobd@gmail.com>
2020-10-30 15:09:05 -0600

    fixes

--- datasets/simple_qr_dataset.py
+++ datasets/simple_qr_dataset.py
@@ -51,7 +51,7 @@ class SimpleQRDataset(Dataset):
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=11,
+            box_size=10,
             border=2,
         )
         if self.indexes is not None:
