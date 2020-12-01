b0f133da9df34bb789aa4d874ec2ec9675313096
Brian Davis <herobd@gmail.com>
2020-12-01 11:00:01 -0700

    Modified pixel loss to give equal weight to corner and center values

--- model/qr_center_pixel_loss.py
+++ model/qr_center_pixel_loss.py
@@ -2,12 +2,18 @@ import torch
 from torch import nn
 
 class QRCenterPixelLoss(nn.Module):
-    def __init__(self,img_size,qr_size,padding,threshold=0,bigger=False):
+    def __init__(self,img_size,qr_size,padding,threshold=0,bigger=False,split=False):
         super(QRCenterPixelLoss, self).__init__()
         assert(qr_size<=33)
         self.threshold=threshold
+        self.split=split
         #create weight mask
         self.mask = torch.FloatTensor(img_size,img_size).zero_()
+        if split:
+            self.mask_corners = torch.FloatTensor(img_size,img_size).zero_()
+            mask_corners = self.mask_corners
+        else:
+            mask_corners = self.mask
         
         cell_size = img_size/(qr_size+2*padding)
         #mask achors (with padding)
@@ -15,28 +21,28 @@ class QRCenterPixelLoss(nn.Module):
         top_left_right_x = round((padding+7+1)*cell_size)
         top_left_top_y = round((padding-1)*cell_size)
         top_left_bot_y = round((padding+7+1)*cell_size)
-        self.mask[top_left_top_y:top_left_bot_y,top_left_left_x:top_left_right_x]=1
+        mask_corners[top_left_top_y:top_left_bot_y,top_left_left_x:top_left_right_x]=1
 
         top_right_left_x = round((padding+qr_size-8)*cell_size)
         top_right_right_x = round((padding+qr_size+1)*cell_size)
         top_right_top_y = round((padding-1)*cell_size)
         top_right_bot_y = round((padding+7+1)*cell_size)
-        self.mask[top_right_top_y:top_right_bot_y,top_right_left_x:top_right_right_x]=1
+        mask_corners[top_right_top_y:top_right_bot_y,top_right_left_x:top_right_right_x]=1
 
         bot_left_left_x = round((padding-1)*cell_size)
         bot_left_right_x = round((padding+7+1)*cell_size)
         bot_left_top_y = round((padding+qr_size-8)*cell_size)
         bot_left_bot_y = round((padding+qr_size+1)*cell_size)
-        self.mask[bot_left_top_y:bot_left_bot_y,bot_left_left_x:bot_left_right_x]=1
+        mask_corners[bot_left_top_y:bot_left_bot_y,bot_left_left_x:bot_left_right_x]=1
 
 
 
-        if qr_size>=25:
+        if qr_size>=25:#bottom right anchor only exists in larged qr codes
             bot_right_left_x = round((padding+qr_size-7 -2)*cell_size)
             bot_right_right_x = round((padding+qr_size-7 +3)*cell_size)
             bot_right_top_y = round((padding+qr_size-7 -2)*cell_size)
             bot_right_bot_y = round((padding+qr_size-7 +3)*cell_size)
-            self.mask[bot_right_top_y:bot_right_bot_y,bot_right_left_x:bot_right_right_x]=1
+            mask_corners[bot_right_top_y:bot_right_bot_y,bot_right_left_x:bot_right_right_x]=1
 
         #mask pixel centers. I'll do full at very center and 0.5 around
         for cell_r in range(qr_size):
@@ -53,6 +59,8 @@ class QRCenterPixelLoss(nn.Module):
                     self.mask[center_y,center_x]=1
         
         self.mask=self.mask[None,...] #batch dim
+        if self.split:
+            self.mask_corners=self.mask_corners[None,...]
 
     def forward(self,pred,gt):
         pred = pred.mean(dim=1) #grayscale!
@@ -60,7 +68,16 @@ class QRCenterPixelLoss(nn.Module):
         gt=gt[:,0]
         diff = torch.abs(pred-gt)
         diff[diff<self.threshold] = 0
-        self.mask = self.mask.to(diff.device)
-        diff*=self.mask
-        return diff.mean()
+        if diff.is_cuda and not self.mask.is_cuda:
+            self.mask = self.mask.to(diff.device)
+            if self.split:
+                self.mask_corners = self.mask_corners.to(diff.device)
+
+        if self.split:
+            diff_corners = diff*self.mask_corners
+            diff_message = diff*self.mask
+            return (diff_corners.sum()/self.mask_corners.sum() + diff_message.sum()/self.mask.sum())/2
+        else:
+            diff*=self.mask
+            return diff.mean()
 
