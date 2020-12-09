b3a560d0edefa7001296aeea0e3002af30ea0879
Taylor Archibald <36053181+Tahlor@users.noreply.github.com>
2020-12-09 12:29:12 -0600

    update configs

--- configs/taylor/gen_configs.py
+++ configs/taylor/gen_configs.py
@@ -5,42 +5,65 @@ from pathlib import Path
 from copy import deepcopy
 from easydict import EasyDict as edict
 import gen_scripts
+
+# configs/brian/cf_preArt_trainDecoder_newPix_maskCorners.json
+
 from time import sleep
 
 out = Path("./auto/")
 out.mkdir(parents=True, exist_ok=True)
-config_prime = edict(json.load(Path("./DEFAULT.conf").open()))
+config_prime = edict(json.load(Path("./DEFAULT.json").open()))
 
-for coord_conv in True,False:
-    for alphabet in [string.digits,string.digits+string.ascii_lowercase]:
-        for architecture in ["default"]: #["resnet", "default"]:
-            config = deepcopy(config_prime)
-            if architecture == "default":
-                config.arch = "DecoderCNN"
-                config.model = {
-                    "input_size": [128, 128],
-                    "cnn_layer_specs": [1, 32, "cc-32", "M", "cc-64", "cc-64", "M", "cc-128", "cc-128", "cc-128", "M",
-                                        "cc-256", "cc-256", "cc-256"],
-                    "fully_connected_specs": ["FC2048", "FC1024"],
-                    "max_message_len": 17,
-                    "num_char_class": 11
-                }
-                config.data_loader.coordconv = False
-                if not coord_conv:
-                    config.model.cnn_layer_specs = [1,32,32,"M",64,64,"M",128,128,128,"M",256,256,256]
-            else:
-                config.data_loader.coordconv = coord_conv
-                config.arch = "ResNet"
-            config.data_loader.alphabet = alphabet
-            config.model.max_message_len = 17
-            config.data_loader.max_message_len = config.model.max_message_len = 17
+datasets = {"art":"../data/abstract_art",
+            "texture":"../data/texture",
+            "cloud":"../data/understanding_cloud_organization",
+            "water":"../data/water",
+            }
 
-            # Make configs consistent
-            config.data_loader.input_size = config.model.input_size
-            config.data_loader.batch_size = 32
-            name = f"{architecture}{'_cconv' if coord_conv else '_'}_{'alphanumeric' if 'a' in alphabet else 'digits'}.json"
-            config.name = Path(name).stem
-            json.dump(config.__dict__,(out / f"___{name}").open("w"), indent=4, separators=(',', ':'))
-            #os.chmod(out / f"___{name}", 755)
+#../data/terrain
+# configs/brian/cf_preArt_trainDecoder_newPix_maskCorners.json
 
-gen_scripts.run_it()
+for coord_conv in False:
+    for hi_res in True, False:
+        for patch_type in "patch", "patch layers6":
+            for dataset in datasets.keys():
+                config = deepcopy(config_prime)
+                config.data_loader.data_dir = datasets[dataset]
+
+                if hi_res:
+                    # delete encoder
+                    config.model.generator = config.model.generator.replace("predict_offset")  # "SG2UGen small coordconv unbound predict_offset"
+                    #config.model.discriminator = "StyleGAN2 smaller mask_corners"
+                    config.model.pretrained_qr = None  # "saved/default_cconv_digits_multimask/checkpoint-latest.pth"
+
+                    # make hi res QR
+                    config.data_loader.QR_dataset.str_len = 58
+                    config.data_loader.data_set_name = "AdvancedQRDataset"
+                    config.data_loader.alphabet_description = "printable"
+                    config.data_loader.distortions = False
+                    config.data_loader.alphabet = string.printable
+                    config.data_loader.error_level = "h"
+
+                    # bigger discriminator / generator
+                    config.data_loader.generator = config.data_loader.generator.replace("small","")
+                    config.data_loader.discriminator = config.data_loader.discriminator.replace("small", "")
+
+                    del config.loss.char
+                    del config.loss.valid
+                    for i,item in enumerate(config.trainer.curriculum["0"][::-1]):
+                        if i == "decoder":
+                            config.trainer.curriculum["0"][::-1].pop(i)
+
+                hi_res = "hi_res" if hi_res else "low_res"
+                name = f"{dataset}_{hi_res}"
+
+                trainer.print_dir = f"train_out/{name}"
+                sample_data_loader.cache_dir = f"cache/{name}"
+                Path(trainer.print_dir).mkdir(parents=True, exist_ok=True)
+                Path(sample_data_loader.cache_dir).mkdir(parents=True, exist_ok=True)
+
+                config.name = Path(name).stem
+                json.dump(config.__dict__,(out / f"___{name}").open("w"), indent=4, separators=(',', ':'))
+                #os.chmod(out / f"___{name}", 755)
+
+gen_slurm_scripts.run_it()
